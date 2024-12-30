f1ab7fa1d7e9f96cc4a351c9ecd753a2066481aa
mathamre <mathias.hamre@gmail.com>
2024-12-30 17:31:44 +0100

    test

--- frontend/src/lib/repository/business.db.ts
+++ frontend/src/lib/repository/business.db.ts
@@ -1,19 +1,21 @@
 import prisma from "@/src/lib/prisma";
-
 interface NewBusinessInput {
-    name: string;
+  name: string;
 }
 
 export async function addBusiness(input: NewBusinessInput) {
-    try {
-        const newBusiness = await prisma.business.create({
-            data: {
-                name: input.name, // Only the `name` field is required
-            },
-        });
-        return newBusiness;
-    } catch (error) {
-        console.error("Error adding business:", error);
-        throw error;
-    }
+  try {
+    const newBusiness = await prisma.business.create({
+      data: {
+        name: input.name,
+        portfolio: {
+          create: {}, // Create an empty portfolio or populate it with initial data
+        },
+      },
+    });
+    return newBusiness;
+  } catch (error) {
+    console.error("Error adding business with portfolio:", error);
+    throw error;
+  }
 }
